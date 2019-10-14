6a79c1ee0493fb69d4e1ab6dcc97ae879a6dee88
Michael Greenbaum <michael.greenbaum@addepar.com>
2019-10-14 15:15:43 -0700

    Making rating boundaries 31337

--- truescrub/matchmaking.py
+++ truescrub/matchmaking.py
@@ -37,6 +37,8 @@ SKILL_GROUPS = ((float('-inf'), SKILL_GROUP_NAMES[0]),) + tuple(
         for i, name in enumerate(SKILL_GROUP_NAMES[1:])
 )
 
+def makeElite(rating):
+  return 1337 if abs(rating - 1337) < SKILL_GROUP_SPACING / 2 else rating
 
 def skill_group_name(mmr: float) -> str:
     group_ranks = [group[0] for group in SKILL_GROUPS]
